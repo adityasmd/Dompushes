4461b1bfd05866ba0dd253d376fbc79516705e2a
Aditya Dubey <dubeyaditya5000@gmail.com>
2025-12-02 00:58:17 +0530

    IPL2026 winner checking game using DOM

--- first.js
+++ first.js
@@ -1,10 +1,50 @@
+
+
+let arr= [
+   {
+      Team:"MI",
+      primary:"blue",
+      secondary:"Gold",
+      Trophies: 5,
+      captain: "Rohit"
+   },
+   {
+      Team:"RCB",
+      primary:"red",
+      secondary:"grey",
+      Trophies: 1,
+      captain: "rajat"
+   },
+   {
+      Team:"CSK",
+      primary:"yellow",
+      secondary:"white",
+      Trophies: 5,
+      captain: "MSD"
+   },
+   {
+      Team:"PK",
+      primary:"crimson",
+      secondary:"grey",
+      Trophies: 0,
+      captain: "sheryash"
+   },
+   {
+      Team:"RR",
+      primary:"pink",
+      secondary:"navyblue",
+      Trophies: 5,
+      captain: "sanju"
+   },
+];
+
 let btn = document.querySelector('button')
 let box = document.querySelector('.box')
-
+let body= document.querySelector('body')
 btn.addEventListener('click', function(){
-   let c1 = Math.floor(Math.random()*256)
-   let c2 = Math.floor(Math.random()*256)
-   let c3 = Math.floor(Math.random()*256)
-   
-   box.style.backgroundColor = `rgb(${c1},${c2}, ${c3})`
+  let win = Math.floor( Math.random()*arr.length);
+  box.innerHTML =  `Team: ${arr[win].Team}  Trophies: ${arr[win].Trophies} captain: ${arr[win].captain}`
+  body.style.backgroundColor = arr[win].primary
+  box.style.backgroundColor = arr[win].secondary
 })
+

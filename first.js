1fe5b36307a0eeb6eb015ae0c754267f628fcb36
Aditya Dubey <dubeyaditya5000@gmail.com>
2025-12-03 20:38:27 +0530

    random name using DOM

--- first.js
+++ first.js
@@ -1,26 +1,28 @@
 let btn= document.querySelector('button')
 let main = document.querySelector('main')
 
-btn.addEventListener('click', function(){
-  let newelem= document.createElement('div');
-   newelem.style.height= 120+'px'
-   newelem.style.width= 120+'px'
-   newelem.style.position= 'absolute'
-   newelem.style.backgroundColor= `rgb(200,200,200)`
+let arr=['hellopajiii','kya heroooooo', 'chal bhaiiiiiiiiii','kya haal chal','bade bhai ke bhai']
 
-   main.appendChild(newelem);
+btn.addEventListener('click', function(){
+   let h1= document.createElement('h1');
+   let i= Math.floor(Math.random()*arr.length)
+
+   h1.innerHTML= arr[i] 
+   main.appendChild(h1)
+   h1.style.position= 'absolute'
+   h1.style.fontSize= 50+'px'
 
    let x = Math.random()*100
    let y = Math.random()*100
    let rotation = Math.random()*360
-
-   newelem.style.left= x+'%'
-   newelem.style.top= y+'%'
-   newelem.style.rotate= rotation+'deg'
+   
+   h1.style.left= x+'%'
+   h1.style.top= y+'%'
+   h1.style.rotate= rotation+'deg'
 
    let c1 = Math.floor(Math.random()*356)
    let c2 = Math.floor(Math.random()*356)
    let c3 = Math.floor(Math.random()*356)
 
-   newelem.style.backgroundColor= `rgb(${c1},${c2},${c3})`
+   h1.style.color= `rgb(${c1},${c2},${c3})`
 })

22c49f77fd4014ce326677c9788873f798468d05
Aditya Dubey <dubeyaditya5000@gmail.com>
2025-12-05 00:37:31 +0530

    unique card showing info using dom

--- first.js
+++ first.js
@@ -1,17 +1,46 @@
-let imgs = document.querySelector('img')
-let love= document.querySelector('#love')
+const people = [
+  {
+    image: "https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    fullName: "Alice Johnson",
+    profession: "Software Engineer",
+    description: "Specializes in backend development and cloud infrastructure."
+  },
+  {
+    image: "https://images.unsplash.com/photo-1544005313-94ddf0286df2",
+    fullName: "Michael Roberts",
+    profession: "Graphic Designer",
+    description: "Creates branding, illustrations, and user interface designs."
+  },
+  {
+    image: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e",
+    fullName: "Sofia Martinez",
+    profession: "Data Analyst",
+    description: "Analyzes datasets to generate insights and strategic recommendations."
+  },
+  {
+    image: "https://plus.unsplash.com/premium_photo-1734026668803-4ab767f3de76?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    fullName: "David Kim",
+    profession: "Marketing Manager",
+    description: "Develops marketing strategies and oversees brand communication."
+  },
+  {
+    image: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e",
+    fullName: "Emma Thompson",
+    profession: "Product Manager",
+    description: "Leads product planning, development, and cross-functional coordination."
+  }
+];
 
-imgs.addEventListener('dblclick', () =>{
-   love.style.opacity= 0.8;
-   love.style.transform= 'translate(-50%,-50%) scale(1)  rotate(0deg)';
 
-   setTimeout(() => {
-       love.style.transform= 'translate(-50%,-177%) scale(1)  rotate(-60deg)';
-   }, 800);
-   setTimeout(() => {
-      love.style.opacity= 0;
-   }, 1100);
-    setTimeout(() => {
-       love.style.transform= 'translate(-50%,-50%) scale(0)  rotate(0deg)';
-   }, 2000);
+let sum= ''
+
+people.forEach(function(elem){
+   sum = sum+` <div class="cards">
+      <img src='${elem.image}'>
+      <h1>'${elem.fullName}'</h1>
+      <h2>'${elem.profession}'</h2>
+      <h3>'${elem.description}'</h3>
+    </div>`
 })
+var main = document.querySelector('main')
+main.innerHTML= sum
